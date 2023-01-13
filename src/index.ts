c9e25afb06b8802532bdb3f3c969d8784e1150b0
Nadire-Nadir <nadiraemam@gmail.com>
2023-01-13 22:14:46 +0200

    setup user authentication flow

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import * as dotenv from 'dotenv'
 import app from './server'
+
+dotenv.config()
 
 app.listen(8000, () => {
     console.log('hello on http://localhost:8000')
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express from 'express'
 import router from './router'
 import morgan from 'morgan'
+import { protect } from './modules/auth'
+import { createNewUser, signin } from './handlers/user'
 
 const app = express()
 
 app.use(morgan('dev'))
+
+// check if these needed in the future
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     console.log('Hello from express')
@@ -12,7 +18,10 @@ app.get('/', (req, res) => {
     res.json({ message: 'Hello' })
 })
 
-app.use('/api', router)
+app.use('/api', protect, router)
+
+app.use('/user', createNewUser)
+app.post('/signin', signin)
 
 
 export default app
