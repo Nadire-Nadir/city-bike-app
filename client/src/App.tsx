f9db5b7c36586f87106089c80357815102db9141
Nadire-Nadir <nadiraemam@gmail.com>
2023-01-24 16:29:27 +0200

    created auth provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
+import { Admin, Resource } from 'react-admin'
+import jsonServerProvider from 'ra-data-json-server'
+import authProvider from './authProvider';
+
+const dataProvider = jsonServerProvider('http://localhost:8000')
 
 const App = () => {
+
   return (
-    <div className='App'>
-      "Hello world"
-    </div>
+    <Admin dataProvider={dataProvider} authProvider={authProvider}>
+      <div className='App'>
+        "Hello world"
+        </div>
+    </Admin>
   );
 };
 
